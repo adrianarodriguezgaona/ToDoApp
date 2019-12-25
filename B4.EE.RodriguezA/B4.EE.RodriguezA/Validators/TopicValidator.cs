d663fbc9afbf2309428462b8f407b7490c23842a
Adriana Rodriguez <adriana@codekick.be>
2019-12-25 10:55:02 +0100

    Map Validator add with TopicValidatorClass

---
+++ B4.EE.RodriguezA/B4.EE.RodriguezA/Validators/TopicValidator.cs
@@ -0,0 +1,13 @@
+﻿using B4.EE.RodriguezA.Domain.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B4.EE.RodriguezA.Validators
+{
+    public class TopicValidator : AbstractValidator<ReminderTopic>
+    {
+
+    }
+}
