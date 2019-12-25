1dc500a9ba89e532beaa3979ea32fc5da9ee25c0
Adriana Rodriguez <adriana@codekick.be>
2019-12-25 10:50:51 +0100

    Class ReminderTopic and TopicItem add ReminderTopic has a list of TopicItem

---
+++ B4.EE.RodriguezA/B4.EE.RodriguezA/Domain/Models/ReminderTopic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B4.EE.RodriguezA.Domain.Models
+{
+    public class ReminderTopic
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<TopicItem> Items { get; set; } = new List<TopicItem>();
+
+    }
+}
---
+++ B4.EE.RodriguezA/B4.EE.RodriguezA/Domain/Models/TopicItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B4.EE.RodriguezA.Domain.Models
+{
+    public class TopicItem
+    {
+        public Guid Id { get; set; }
+        public Guid ReminderTopicId { get; set; }
+        public ReminderTopic ParentTopic { get; set; }
+        public string ItemName { get; set; }
+        public string PhotoSource { get; set; }
+        public string MyTopicItem { get; set; }
+
+        public DateTime? ToDoDate { get; set; }
+    }
+}
