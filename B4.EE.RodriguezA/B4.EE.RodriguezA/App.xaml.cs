4a1e171a7002d65b327afc59cd07f59fa1481e11
Adriana Rodriguez <adriana@codekick.be>
2019-12-26 09:29:58 +0100

    New route add for use the MainViewModel with MVVM, Events , appstyles and layout register

--- B4.EE.RodriguezA/B4.EE.RodriguezA/App.xaml.cs
+++ B4.EE.RodriguezA/B4.EE.RodriguezA/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using B4.EE.RodriguezA.Domain.Services;
+using B4.EE.RodriguezA.Domain.Services.FileIO;
+using B4.EE.RodriguezA.ViewModels;
+using FreshMvvm;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +14,12 @@ namespace B4.EE.RodriguezA
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTUyNzA0QDMxMzcyZTMzMmUzME5PemVVY2JiQWtqSHZnLzZtRHRVdVRVc3pxVjU5R1kyN2pGNjNyUnBLSGM9");
+
+            //Register dependencies
+            FreshIOC.Container.Register<ITopicRepository>(new JsonTopicRepository());
+
+            MainPage = new FreshNavigationContainer(FreshPageModelResolver.ResolvePageModel<MainViewModel>());
         }
 
         protected override void OnStart()
