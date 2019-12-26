fa1c57e3dfcecc7629cd99ad87368a273226d728
Adriana Rodriguez <adriana@codekick.be>
2019-12-26 09:32:30 +0100

    Changes in AndroidManifest and Assembly to use media.plugin in Android UWp permission webcam add Fotos add to use in the pages

--- B4.EE.RodriguezA/B4.EE.RodriguezA.Android/MainActivity.cs
+++ B4.EE.RodriguezA/B4.EE.RodriguezA.Android/MainActivity.cs
@@ -23,11 +23,16 @@ namespace B4.EE.RodriguezA.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
