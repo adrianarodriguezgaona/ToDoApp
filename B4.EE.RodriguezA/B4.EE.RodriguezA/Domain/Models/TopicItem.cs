47798e97007d2959d9e792bcdd5f7af6afea1768
Adriana Rodriguez <adriana@codekick.be>
2019-12-29 18:12:17 +0100

    New properties add to TopicItem to have a selected time, date to string, a color for prior, is prior and a description

--- B4.EE.RodriguezA/B4.EE.RodriguezA/Domain/Models/TopicItem.cs
+++ B4.EE.RodriguezA/B4.EE.RodriguezA/Domain/Models/TopicItem.cs
@@ -12,7 +12,13 @@ namespace B4.EE.RodriguezA.Domain.Models
         public string ItemName { get; set; }
         public string PhotoSource { get; set; }
         public string MyTopicItem { get; set; }
-
+        public bool IsPrior { get; set; }
+        public string Description { get; set; }
         public DateTime? ToDoDate { get; set; }
+        public string ToDoDateString { get; set; }
+        public TimeSpan SelectedTime { get; set; }
+
+        public string ColorForPrior { get; set; }
+
     }
 }
